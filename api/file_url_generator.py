0d40f8bcc578eea6a0e880cbc09f2b4047f65cbc
thejas98 <thejasbharadwaj98@gmail.com>
2023-02-23 18:36:54 -0500

    Update file_url_generator.py

--- api/file_url_generator.py
+++ api/file_url_generator.py
@@ -17,9 +17,9 @@ from fastapi.templating import Jinja2Templates
 from typing import Optional
 from urllib.parse import quote
 from typing import Dict, Any
-
-from jwt import User, get_current_active_user
-
+import jwt
+User = jwt.User
+get_current_active_user = jwt.get_current_active_user
 router_file_url_generator = APIRouter()
 
 clientlogs = boto3.client('logs',
@@ -148,12 +148,29 @@ def filename_url_gen_nexrad(filename: str,current_user: User = Depends(get_curre
 
 
 
+@router_file_url_generator.get('/url_generator_geos')
+def url_gen_goes(input:str, current_user: User = Depends(get_current_active_user)):
+    write_logs(message="url_generator_starts")
+    arr = input.split("_")
+    tproduct_code = arr[1].split("-")
+    s1 = tproduct_code[2]
+    finalProductCode =tproduct_code[0]+"-"+tproduct_code[1]+"-"+ ''.join([i for i in s1 if not i.isdigit()])
+    write_logs(finalProductCode)
+    date = arr[3]
+    year, day_of_year, hour = date[1:5], date[5:8], date[8:10]
+    fs = "https://noaa-goes18.s3.amazonaws.com/{}/{}/{}/{}/{}".format(finalProductCode,year,day_of_year,hour,input)
+    write_logs(fs)
+    return {"url":fs}
 
 
 
-
-
-
+@router_file_url_generator.get('/url_generator_nexrad')
+def url_gen_nexrad(input):
+    arr = input.split("_")[0]
+    year, day, hour, station = arr[4:8], arr[8:10], arr[10:12], arr[0:4]
+    fs = "https://noaa-nexrad-level2.s3.amazonaws.com/{}/{}/{}/{}/{}".format(year,day,hour,station,input)
+    write_logs(fs)
+    return {"url":fs}
 
 
 
