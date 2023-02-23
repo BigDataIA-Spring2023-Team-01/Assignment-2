396cec6240ad9bb7533adb8b84820900d04bf70f
Anandita <deb.ana@northeastern.edu>
2023-02-23 15:05:11 -0500

    added router_file_nexrad

--- api/mainAPI.py
+++ api/mainAPI.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI,Response
 from fastapi.responses import JSONResponse
-import jwt, metadata_geos,metadata_nexrad,file_url_generator,nexrad_coords,file_url_generator,file_transfer
+import jwt, metadata_geos,metadata_nexrad,file_url_generator,nexrad_coords,file_url_generator,file_transfer,file_transfer_nexrad
 
 app = FastAPI()
 
@@ -9,8 +9,8 @@ app.include_router(metadata_geos.router_metadata_geos)
 app.include_router(metadata_nexrad.router_metadata_nexrad)
 app.include_router(file_url_generator.router_file_url_generator)
 app.include_router(file_transfer.router_file_transfer)
+app.include_router(file_transfer_nexrad.router_file_transfer_nexrad)
 app.include_router(nexrad_coords.router_nexrad_coords)
-
 
 #app.default_response_class = JSONResponse
 
