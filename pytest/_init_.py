e1a56160a4d6abe42538f31b94e58f233a958da4
thejas98 <thejasbharadwaj98@gmail.com>
2023-02-24 02:04:35 -0500

    Delete _init_.py

--- pytest/_init_.py
+++
@@ -1,4 +0,0 @@
-__all__ = ['file_url_generator_test_geos', 'file_url_generator_test_nexrad','demotest']
-# deprecated to keep older scripts who import this from breaking
-
-import file_url_generator_test_geos,file_url_generator_test_nexrad,demotest
