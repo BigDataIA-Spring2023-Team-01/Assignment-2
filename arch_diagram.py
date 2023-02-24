9c0ef23c5b4a9f6f1cecf4aa4dbcfc097ad70b95
CY2404 <114796225+CY2404@users.noreply.github.com>
2023-02-24 01:43:47 -0500

    Architecture Diagram

---
+++ arch_diagram.py
@@ -0,0 +1,7 @@
+from diagrams import Diagram
+from diagrams.azure.identity import Users
+from diagrams.onprem.container import Docker
+from diagrams.onprem.workflow import Airflow
+from diagrams.aws.storage import SimpleStorageServiceS3 as S3
+from diagrams.onprem.database import Mssql as MSSQL 
+from diagrams.oci.monitoring import Telemetry
